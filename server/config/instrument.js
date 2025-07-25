e4b50fc98af0581ef9f9f31e8bfb5c6aec390a83
updkri <159511919+updkri@users.noreply.github.com>
2025-07-25 10:55:14 +0530

    Add files via upload

---
+++ server/config/instrument.js
@@ -0,0 +1,29 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://bce0ef8a5f8688e1b0962d9f3d98502a@o4508946619891712.ingest.us.sentry.io/4509264251977728",
+  integrations: [
+    nodeProfilingIntegration(),
+    Sentry.mongooseIntegration()
+  ],
+  // Tracing
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
+// Profiling happens automatically after setting it up with `Sentry.init()`.
+// All spans (unless those discarded by sampling) will have profiling data attached to them.
+Sentry.startSpan({
+  name: "My Span",
+}, () => {
+  // The code executed here will be profiled
+});
